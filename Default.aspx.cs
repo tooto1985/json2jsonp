f42b9f3a98a70bf1ebaacf41b0f2d05851ee689e
tommy <tooto1985@gmail.com>
2016-08-05 10:01:12 +0800

    add

---
+++ Default.aspx.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.IO;
+using System.Net;
+
+public partial class _Default : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        var url = Request.QueryString["url"];
+        var callback = Request.QueryString["callback"];
+        if (!string.IsNullOrEmpty(url))
+        {
+            var hwr = WebRequest.Create(url).GetResponse();
+            var stream = hwr.GetResponseStream();
+            if (stream != null)
+            {
+                using (var sr = new StreamReader(stream))
+                {
+                    var format = "{0}";
+                    var contentType = "application/json";
+                    if (!string.IsNullOrEmpty(callback))
+                    {
+                        format = "{1}({0})";
+                        contentType = "application/javascript";
+                    }
+                    Response.ContentType = contentType;
+                    Response.Write(string.Format(format, sr.ReadToEnd(), callback));
+                }
+            }
+        }
+        Response.End();
+    }
+}
